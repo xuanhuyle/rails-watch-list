dac1e844ef28059d2fe46aadd0f8ab703a41722d
xuan huy le <xuanhuyle@gmail.com>
2024-07-04 16:13:43 +0200

    movies

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :movies, only: [:index, :show, :new, :create, :destroy]
-  resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:create, :new, :destroy]
+  resources :bookmarks, only: [:destroy]
+  resources :lists do
+    resources :bookmarks, only: [:create, :new]
   end
 
   root to: 'lists#index'
