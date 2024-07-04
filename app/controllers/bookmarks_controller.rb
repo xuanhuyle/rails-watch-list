aaf7cffaa425ab33a170bd15589d6a6e8a70d32a
xuan huy le <xuanhuyle@gmail.com>
2024-07-04 15:13:51 +0200

    movies

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,11 +1,18 @@
 class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+    @movies = Movie.all # This will be used to populate the dropdown of movies
+  end
+
   def create
     @list = List.find(params[:list_id])
     @bookmark = @list.bookmarks.new(bookmark_params)
     if @bookmark.save
-      redirect_to @list
+      redirect_to @list, notice: 'Bookmark was successfully created.'
     else
-      render 'lists/show'
+      @movies = Movie.all
+      render :new
     end
   end
 
@@ -13,7 +20,7 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.find(params[:id])
     list = @bookmark.list
     @bookmark.destroy
-    redirect_to list
+    redirect_to list, notice: 'Bookmark was successfully deleted.'
   end
 
   private
